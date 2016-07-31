e78500720d921d6326b4a56d4ea1b95c3b0540c9
KemalC <ddeveloperr@gmail.com>
2016-07-31 14:43:14 +0000

    Create a name registration field

--- db/migrate/20160731104545_devise_create_users.rb
+++ db/migrate/20160731104545_devise_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      #Username
+      #Add Username 
       t.string :name
       
       ## Database authenticatable
